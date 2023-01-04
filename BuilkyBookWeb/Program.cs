709c8e88315bb97f6c53bee7cb6d4c9fd65e6eeb
LSGeonmin <lkm9709@portlogics.com>
2023-01-04 14:24:16 +0900

    docs: Program.cs 주석

--- BuilkyBookWeb/Program.cs
+++ BuilkyBookWeb/Program.cs
@@ -1,11 +1,16 @@
+// entry point>?
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// mvc를 위한 서비스  
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// request로부터 이어지는 파이프라인 
+// 순서가 어느정도 정해져있음
+// 빌더를 통해 서비스를 구축, 파이프라인 생성 후 app.Run()
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -20,6 +25,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// 라우팅
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
