a88ecb080cc8a01d598e80f755816af33fe24678
LSGeonmin <lkm9709@portlogics.com>
2023-01-05 17:51:29 +0900

    feat: dbContext를 사용해 Category db read

---
+++ BuilkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using BuilkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuilkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // ApplicationDbContext를 Program.cs에서 DI하였기 때문에 그냥 가져와서 사용가능
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) { _db = db; }
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+
+            return View();
+        }
+    }
+}
