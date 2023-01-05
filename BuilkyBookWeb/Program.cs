3ad0c2a3a7b3f7d5b2b835c1310c12c265c57d90
LSGeonmin <lkm9709@portlogics.com>
2023-01-05 17:24:32 +0900

    feat: sqlserver와 연결

--- BuilkyBookWeb/Program.cs
+++ BuilkyBookWeb/Program.cs
@@ -1,9 +1,17 @@
 // entry point>?
+using BuilkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // mvc를 위한 서비스  
 builder.Services.AddControllersWithViews();
+//dbContext와 연결하기 위한 서비스 
+//ConnectionStrings은 appsettings.json에서 가져옴 
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
